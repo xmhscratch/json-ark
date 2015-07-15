7ff9b352a2f912d5ed60ef90a8be08cb31e7964e
xmhscratch <xmhscratch@gmail.com>
2015-07-15 21:50:34 +0700

    update index.js

--- index.js
+++ index.js
@@ -2,25 +2,55 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
-module.exports = function(filePath, data, options, done) {
+var JSONScore = function(filepath, options) {
 	options = (options || {});
-	_.defaults(options, {
+	_.defaults(options, this.options);
+	this.options = options;
+
+	this.filepath = path.normalize(filepath);
+
+	_.mixin({
+		write: this.write.bind(this)
+	});
+
+	return this.read();
+}
+
+JSONScore.prototype = {
+	filepath: null,
+
+	options: {
 		encoding: 'utf-8',
 		replacer: null,
 		space: null
-	});
+	},
 
-	filePath = path.normalize(filePath);
-	fs.readFile(filePath, { encoding: options.encoding }, function(error, content) {
-		if (error) {
-			return done(error, data);
-		}
+	read: function() {
+		var data = fs.readFileSync(this.filepath, {
+			encoding: this.options.encoding
+		});
 
-		data = _.extend({}, data, JSON.parse(content));
-        fs.writeFile(filePath, JSON.stringify(
-        	data, options.replacer, options.space
-    	), function(error) {
+		if (Buffer.isBuffer(data)) {
+			data = data.toString('utf8');
+		};
+
+		return _(JSON.parse(data)).chain();
+	},
+
+	write: function(data, done) {
+		data = data.value();
+
+		fs.writeFile(this.filepath, JSON.stringify(
+			data, this.options.replacer, this.options.space
+		), function(error) {
 			return done.call(error, data);
 		});
-	});
+
+		return data;
+	}
+}
+
+module.exports = function(filepath, options) {
+	var jsonScore = new JSONScore(filepath, options);
+	return jsonScore;
 }
